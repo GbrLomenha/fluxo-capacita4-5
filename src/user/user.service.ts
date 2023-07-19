a92c72063649b3c46cb185f702d52e1214bc8595
Gabriel Lomenha <gabriellomenha@gmail.com>
2023-07-19 14:35:22 -0300

    Criação de novo usuário com senha hasheada

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,14 +4,23 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor( @InjectRepository(User) private userRepository: Repository<User>) {}
 
-
+  //Cria novo Usuário
   async create(createUserDto: CreateUserDto){
-      
+    const hashed_password = await this.hashPassword(createUserDto.password); //Hasheia a senha cadastrada
+
+    delete createUserDto.password; //Deleta a senha inserida
+
+    createUserDto.password = hashed_password  //reatribui a senha hasheada a dto
+
+    const newUser = this.userRepository.create({...createUserDto,}) //Cria o novo usuário
+
+    return this.userRepository.save(newUser); //Salva
   }
 
   findAll() {
@@ -28,5 +37,10 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async hashPassword(password:string): Promise<string>{
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(password,salt);
   }
 }
